3a5bbf7f75a0385419571decb65e25ed6789c23e
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-02-10 08:32:23 -0700

    created renamed to mission, added components for article

---
+++ client/src/components/Article.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ArticleTitle from './ArticleTitle'
+import ArticleContent from './ArticleContent'
+
+export default function Article() {
+    return (
+        <div className='article'>
+            <ArticleTitle />
+            <ArticleContent />
+        </div>
+    )
+}
