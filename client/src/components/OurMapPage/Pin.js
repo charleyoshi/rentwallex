9e856b49fc796237a06c72256388b59b1278ed81
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-02-20 12:17:19 -0700

    last commit

--- client/src/components/OurMapPage/Pin.js
+++ client/src/components/OurMapPage/Pin.js
@@ -5,14 +5,14 @@ const Pin = ({ text, tooltip, $hover }) => {
     console.log(`You clicked on ${tooltip}`);
   };
 
-    return (
-      <div className="pin">
-    <div className={$hover ? "circle hover" : "circle"} onClick={handleClick}>
-      <span className="circleText" title={tooltip}>
-        2
-      </span>
-            </div>
-            </div>
+  return (
+    <div className="pin">
+      <div className={$hover ? "circle hover" : "circle"} onClick={handleClick}>
+        <span className="circleText" title={tooltip}>
+          {text}
+        </span>
+      </div>
+    </div>
   );
 };
 
