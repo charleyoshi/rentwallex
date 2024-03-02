d4c6c3b8eb9c68d44f8a4970f98864064d5783a8
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-03-02 17:33:16 +0800

    add facebook

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RentwallexLogo from '../assets/Rentwallex_Logo_3.png';
 import linkedIn from '../assets/linkedin.png';
-// import facebook from '../assets/facebook.png';
+import Facebook from '../assets/facebook';
 // import instagram from '../assets/instagram.png';
 // import twitter from '../assets/twitter.png';
 import { Link } from 'react-router-dom';
@@ -31,7 +31,7 @@ export default function Footer() {
         {/* Right section */}
         <div className='right'>
           <img src={linkedIn} alt="LinkedIn" style={iconsStyle} />
-          {/* <img src={facebook} alt="Facebook" style={iconsStyle} /> */}
+          <img src={Facebook} alt="Facebook" style={iconsStyle} />
           {/* <img src={instagram} alt="Instagram" style={iconsStyle} />
           <img src={twitter} alt="Twitter" style={iconsStyle} /> */}
         </div>
