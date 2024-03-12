d8bf4d5f92e9a8c16d21d8ee48580586f26a71b4
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-03-12 17:34:14 -0600

    bold titles

--- client/src/components/Article/ArticleContent.js
+++ client/src/components/Article/ArticleContent.js
@@ -4,17 +4,19 @@ export default function ArticleContent() {
     return (
         <div className='articleContent'>
 
-            <h1>We are changing the way people think about rent payment</h1>
+            <h1>We are changing the way people think about
+                <br />
+                rent payment</h1>
             <br />
             <h5>Rentwallex is a forward-thinking financial technology company dedicated to transforming the way people pay rent. We believe in making the rental process more affordable, convenient, and empowering for tenants. With a commitment to financial well-being, we strive to bridge the gap between renters and landlords, fostering a harmonious relationship across communities. </h5>
             
-            <br /><br /><hr /><br /><br />
+            <br /><br /><br /><br />
 
             <h3>Our Mission</h3>
             <br />
             <h5>Our mission is clear – we aim to provide a comprehensive solution that not only simplifies rent payments but also contributes to the overall financial health of our community members. Through innovative rent financing and collaboration with property managers, we aspire to make renting a positive and rewarding experience.</h5>
             
-            <br /><br /><hr /><br /><br />
+            <br /><br /><br /><br />
 
             <h3>Why Rentwallex?</h3><br />
             <ul>
@@ -24,12 +26,12 @@ export default function ArticleContent() {
                 <li>Innovation: At Rentwallex, we're committed to staying at the forefront of financial technology, bringing you cutting-edge solutions for a modern and stress-free renting experience.</li>
             </ul>
             
-            <br /><br /><hr /><br /><br />
+            <br /><br /><br /><br />
             
             <h3>It’s time for renting payment to change.</h3><br />
             <h5>Embark on a journey where renting is not just a transaction but a positive and empowering part of your financial story. Welcome to a new era in renting – welcome to Rentwallex!</h5>
             
-            <br /><br /><hr /><br /><br />
+            <br /><br /><br /><br />
         </div>
     )
 }
