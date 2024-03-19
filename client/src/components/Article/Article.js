0c4fb6261786acf5ad886c0715fce229f118cce5
Shahil Prasad <s.prasad230@mybvc.ca>
2024-03-19 16:25:20 -0600

    UseCasePageFixed

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -1,16 +1,10 @@
 import React from 'react'
-
-import ArticleTitle from './ArticleTitle'
 import ArticleContent from './ArticleContent'
 import UseCaseContent from './UseCaseContent'
-import ArticleSocialMediaContainer from './ArticleSocialMediaContainer'
-
 
 export default function Article(props) {
     return (
         <div className='article'>
-
-            <ArticleTitle title={props.title} />
 
             <div className='articleContentWrapper'>
                 {/* <ArticleSocialMediaContainer /> */}
