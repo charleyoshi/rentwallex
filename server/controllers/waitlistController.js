6bd65f56bcba6cbcd0ddaf62bbb379d9c36018b1
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-03-21 20:45:26 -0600

    backend: /get route for waitlist

--- server/controllers/waitlistController.js
+++ server/controllers/waitlistController.js
@@ -1,12 +1,25 @@
 import firebaseApp from "../config.js";
-import { getDatabase, ref, set, push } from "firebase/database";
+import { getDatabase, ref, set, push, get } from "firebase/database";
 
 // Initialize Realtime Database and get a reference to the service
 const db = getDatabase(firebaseApp);
 const waitlistRef = ref(db, 'waitlist');
 
 const getWaitlist = async (req, res) => {
-    res.send({ message: 'Waitlist get' });
+
+    get(waitlistRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            var result = snapshot.val();
+            res.status(200).json({ result: result });
+        } else {
+            console.log("No data available");
+            res.status(404).json({ error: "No data available" });
+        }
+    }).catch((error) => {
+        res.status(400).json({ error: error.message });
+    });
+
+    
 }
 
 
@@ -16,13 +29,13 @@ const joinWaitlist = async (req, res) => {
 
     try {
         set(newRef, { ...data });
-        res.status(201).json({ message: "new item created"})
-    } catch (error){
+        res.status(201).json({ message: "new item created" })
+    } catch (error) {
         res.status(400).json({ name: error.name, error: error.message })
     }
 
 
-    
+
 }
 
 export { getWaitlist, joinWaitlist }
