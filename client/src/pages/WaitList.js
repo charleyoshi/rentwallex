c4d880f02a4f549caef0252a2a9ab6ce8d1da152
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-03-22 13:30:42 -0600

    fixed axios post request error

--- client/src/pages/WaitList.js
+++ client/src/pages/WaitList.js
@@ -46,141 +46,141 @@ export default function WaitList() {
     }
 
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(formData)
-        axios.post(`http://localhost:4000/api/waitlist`, {
-            ...formData
-        })
-            .then((response) => {
-                console.log(response)
-                // alert('Form has been successfully sent.')
-                setFormData(resetForm)
-                document.querySelector("#manualAddress").value = "";
-            })
-            .catch((error) => {
-                // alert('Something went wrong. Please try again later.')                
+
+        try {
+            const response = await axios.post(`http://localhost:4000/api/waitlist`, { ...formData })
+            console.log(response)
+            setFormData(resetForm)
+            document.querySelector("#manualAddress").value = "";
+        } catch (error) {
+            if (error.response) {
                 console.log(error.response.data) // e.g. invalid address
-            })
+            } else {
+                console.log(error.message)
+            }
+        }
 
-        // console.log(formData)
+    // console.log(formData)
 
-        // if (handleValidation()) {
-        //     alert("Form submitted");
-        // } else {
-        //     alert("Form has errors.")
-        // }
-    }
+    // if (handleValidation()) {
+    //     alert("Form submitted");
+    // } else {
+    //     alert("Form has errors.")
+    // }
+}
 
-    return (
-        <div className='waitlist'>
-            <Navbar />
-            <div className='pageWrapper'>
-                <div className='left'>
-                    <h3>Join Rentwallex community to revolutionize e rent payment</h3>
-                    <br /><br />
-                    <h4>Lorem ipsum dolor sit
-                        amet, consectetur
-                        adipiscing elit, sed do
-                        eiusmod tempor incididunt
-                        ut labore et dolore magna
-                        aliqua. Ut enim ad minim
-                        veniam, quis nostrud
-                        exercitation ullamco
-                        laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis
-                        aute irure dolor in
-                        reprehenderit in voluptate
-                        velit esse cillum dolore eu
-                        fugiat nulla pariatur.
-                        Excepteur sint occaecat
-                        cupidatat non proident,
-                        sunt in culpa qui officia
-                        deserunt mollit anim id est
-                        laborum."</h4>
+return (
+    <div className='waitlist'>
+        <Navbar />
+        <div className='pageWrapper'>
+            <div className='left'>
+                <h3>Join Rentwallex community to revolutionize e rent payment</h3>
+                <br /><br />
+                <h4>Lorem ipsum dolor sit
+                    amet, consectetur
+                    adipiscing elit, sed do
+                    eiusmod tempor incididunt
+                    ut labore et dolore magna
+                    aliqua. Ut enim ad minim
+                    veniam, quis nostrud
+                    exercitation ullamco
+                    laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis
+                    aute irure dolor in
+                    reprehenderit in voluptate
+                    velit esse cillum dolore eu
+                    fugiat nulla pariatur.
+                    Excepteur sint occaecat
+                    cupidatat non proident,
+                    sunt in culpa qui officia
+                    deserunt mollit anim id est
+                    laborum."</h4>
 
-                </div>
-                <div className='right'>
-                    <h1>Join the wait list</h1>
-                    <br /><br />
-                    <form onSubmit={e => handleSubmit(e)}>
-                        <h2>1 - Address</h2>
-
-                        <div className="field">
-                            <label>Property address</label>
-                            <input
-                                ref={addressRef}
-                                name="manualAddress"
-                                id="manualAddress"
-                                required
-                                autoComplete="off"
-                            // onChange={handleChange}
-                            />
-                        </div>
-                        <br />
-                        <div className="field half">
-                            <label>Property manager name</label>
-                            <input
-                                ref={addressNextRef}
-                                name="propertyManagerName"
-                                type="text"
-                                value={formData.propertyManagerName}
-                                required onChange={handleChange} />
-                        </div>
-                        <br /><br />
-                        <h2>2 - Lease</h2>
-                        <div className="field">
-                            <label>Rental cost</label>
-                            <input
-                                name="rentalCost"
-                                type="number"
-                                min="0"
-                                value={formData.rentalCost}
-                                onChange={handleChange} required />
-                        </div>
-                        <br />
-                        <div className="field">
-                            <label>Wage payment frequency</label>
-                            <input
-                                name="wagePaymentFrequency"
-                                type="text"
-                                value={formData.wagePaymentFrequency}
-                                onChange={handleChange} required />
-                        </div>
-                        <br /><br />
-                        <h2>3 - Identification</h2>
-                        <div className="field half">
-                            <label>Email</label>
-                            <input
-                                name="email"
-                                type="email"
-                                value={formData.email}
-                                onChange={handleChange} required />
-                        </div>
-                        <br />
-                        <div className="field">
-                            <label>Employment Status</label><br /><br />
-                            <select value={formData.employmentStatus} onChange={(event) => setFormData({ ...formData, employmentStatus: event.target.value })}>
-                                <option value="fulltimeemployee">Full-time employee</option>
-                                <option value="partimeemployee">Part-time employee</option>
-                                <option value='independentcontractor'>Independent contractor</option>
-                                <option value="retired">Retired</option>
-                                <option value="unemployed">Unemployed</option>
-                            </select>
-                        </div>
-
-
-                        <br /><br /><br />
-
-
-
-
-                        <Button text="Submit" />
-                    </form>
-
-                </div>
             </div>
+            <div className='right'>
+                <h1>Join the wait list</h1>
+                <br /><br />
+                <form onSubmit={e => handleSubmit(e)}>
+                    <h2>1 - Address</h2>
 
+                    <div className="field">
+                        <label>Property address</label>
+                        <input
+                            ref={addressRef}
+                            name="manualAddress"
+                            id="manualAddress"
+                            required
+                            autoComplete="off"
+                        // onChange={handleChange}
+                        />
+                    </div>
+                    <br />
+                    <div className="field half">
+                        <label>Property manager name</label>
+                        <input
+                            ref={addressNextRef}
+                            name="propertyManagerName"
+                            type="text"
+                            value={formData.propertyManagerName}
+                            required onChange={handleChange} />
+                    </div>
+                    <br /><br />
+                    <h2>2 - Lease</h2>
+                    <div className="field">
+                        <label>Rental cost</label>
+                        <input
+                            name="rentalCost"
+                            type="number"
+                            min="0"
+                            value={formData.rentalCost}
+                            onChange={handleChange} required />
+                    </div>
+                    <br />
+                    <div className="field">
+                        <label>Wage payment frequency</label>
+                        <input
+                            name="wagePaymentFrequency"
+                            type="text"
+                            value={formData.wagePaymentFrequency}
+                            onChange={handleChange} required />
+                    </div>
+                    <br /><br />
+                    <h2>3 - Identification</h2>
+                    <div className="field half">
+                        <label>Email</label>
+                        <input
+                            name="email"
+                            type="email"
+                            value={formData.email}
+                            onChange={handleChange} required />
+                    </div>
+                    <br />
+                    <div className="field">
+                        <label>Employment Status</label><br /><br />
+                        <select value={formData.employmentStatus} onChange={(event) => setFormData({ ...formData, employmentStatus: event.target.value })}>
+                            <option value="fulltimeemployee">Full-time employee</option>
+                            <option value="partimeemployee">Part-time employee</option>
+                            <option value='independentcontractor'>Independent contractor</option>
+                            <option value="retired">Retired</option>
+                            <option value="unemployed">Unemployed</option>
+                        </select>
+                    </div>
+
+
+                    <br /><br /><br />
+
+
+
+
+                    <Button text="Submit" />
+                </form>
+
+            </div>
         </div>
-    )
+
+    </div>
+)
 }
