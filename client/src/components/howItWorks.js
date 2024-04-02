fd34b6e11973c04ea391de18663eb02cff1658ab
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-04-02 10:13:48 -0600

    5 real images for home page, fix image size

--- client/src/components/howItWorks.js
+++ client/src/components/howItWorks.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import "../styles/howItWorks.css";
 import dummy_picture from '../assets/picture_placeholder.png'
+import homepage_howitworks_1 from '../assets/homepage_howitworks_1.png'
+import homepage_howitworks_2 from '../assets/homepage_howitworks_2.png'
+import homepage_howitworks_3 from '../assets/homepage_howitworks_3.png'
+import homepage_howitworks_4 from '../assets/homepage_howitworks_4.png'
 function HowItWorks() {
-  
+
 
   return (
     <div className="howItWorks">
@@ -13,36 +17,40 @@ function HowItWorks() {
       <div className="steps">
         <div className="step">
           {/* <p>Step 1</p> */}
-          <img src={dummy_picture} alt="dummy_picture" />
+          <div className="iconWrapper">
+            <img src={homepage_howitworks_1} alt="How it works - 1" />
+          </div>
           <h3 style={{ fontWeight: 'bold' }}>Get Approved</h3>
           <h4>Sign up in minutes. Our simple approval process ensures hassle-free
             access to our community financing solutions.</h4>
         </div>
-        <br className='linebreak'/>
-        <br className='linebreak'/>
+        <br className='linebreak' />
+        <br className='linebreak' />
         <div className="step">
-          <img src={dummy_picture} alt="dummy_picture" />
+          <div className="iconWrapper">
+            <img src={homepage_howitworks_2} alt="How it works - 2" />
+          </div>
           <h3 style={{ fontWeight: 'bold' }}>Customize your payment</h3>
           <h4>Select the rent payment plan that best fits your budget and
-            preferences. We help you to customize repayment based on your pay slip, ensuring a
-            seamless alignment with your income.</h4>
+            preferences. Rentwallex provides interest free, community financing that aligns with your income.</h4>
         </div>
-        <br className='linebreak'/>
-        <br className='linebreak'/>
+        <br className='linebreak' />
+        <br className='linebreak' />
         <div className="step">
-          <img src={dummy_picture} alt="dummy_picture" />
+          <div className="iconWrapper">
+            <img src={homepage_howitworks_3} alt="How it works - 3" />
+          </div>
           <h3 style={{ fontWeight: 'bold' }}>Seamless rent payment</h3>
-          <h4>Once your payment plan is set, Rentwallex facilitates
-            automated, no-interest community financing. Your rent is paid directly to your property
-            manager or registered landlord in full. No late payment. No harassment.</h4>
+          <h4>Your rent is paid directly to your property manager or registered landlord in full. No late payment. No harassment.</h4>
         </div>
-        <br className='linebreak'/>
-        <br className='linebreak'/>
+        <br className='linebreak' />
+        <br className='linebreak' />
         <div className="step">
-          <img src={dummy_picture} alt="dummy_picture" />
+          <div className="iconWrapper">
+            <img src={homepage_howitworks_4} alt="How it works - 4" />
+          </div>
           <h3 style={{ fontWeight: 'bold' }}>Enjoy true financial freedom</h3>
-          <h4>Experience the freedom of manageable rent payments,
-            allowing you to focus on what matters most.</h4>
+          <h4>Experience the freedom of manageable rent payments, allowing you to focus on what matters most.</h4>
         </div>
       </div>
     </div>
