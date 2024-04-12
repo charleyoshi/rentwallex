2584ab01082c3477f550b87516e46f44c2fbd6d4
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-04-11 21:07:33 -0600

    fix property manager form, minor typo in banner

--- client/src/components/OurMapPage/MapPageTopBanner.js
+++ client/src/components/OurMapPage/MapPageTopBanner.js
@@ -3,28 +3,14 @@ import mappage_banner from '../../assets/mappage_banner.jpg'
 
 export default function MapPageTopBanner() {
     return (
-        // <div className='mapPageTopBanner'>
-        //     <div className='left'>
-        //         <span>150+</span>
-        //     </div>
-        //     <div className='right'>
-        //         <span>Renters from across Canada are excited to try Rentwallex.</span>
-        //     </div>
-        // </div>
 
         <div className='headlineBanner'>
             <img src={mappage_banner} alt="Map page banner" />
             <div className='mapPageTopBanner'>
-                {/* <div className='left'>
-                    <span>150+</span>
-                </div> */}
                 <div className='right'>
-                    <span>Renters from across Canada are
-                        {/* <br /> */}
-                        excited to try Rentwallex.</span>
+                    <span>Renters from across Canada are excited to try Rentwallex.</span>
                 </div>
             </div>
-            {/* <h1> 150+ Renters from across Canada are excited to try Rentwallex.</h1> */}
             <br /><br />
         </div>
 
